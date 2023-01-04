8bd0a92df5b864bbc070867492ea1360b5a204fc
KMalveaux <KMalveaux0047@outlook.com>
2023-01-04 16:13:48 -0600

    Added go back button, functionalty, and animations accordingly

--- App.js
+++ App.js
@@ -1,22 +1,44 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useRef } from "react";
-import { StyleSheet, Text, TextInput, View, Animated } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Animated,
+  Button,
+} from "react-native";
 import MyPressable from "./myPressable";
 
 export default function App() {
-  const [Input, onInputChange] = useState();
-  const swingAnimation = useRef(new Animated.Value(0)).current;
+  const [Input, onInputChange] = useState(); // This is the first input the user gives. The "Word" to be given a defition.
+  const [changeable, setChangable] = useState("Word"); //This is the text the changes between "word" and "definition"
+  const slideAnimation = useRef(new Animated.Value(0)).current;
 
-  const swing = () => {
-    Animated.timing(swingAnimation, {
-      toValue: 270,
-      duration: 1000,
-    }).start();
+  const slide = () => {
+    // An animation that moves the word "Word" or "Definition" to the left or to the right.
+    if (changeable == "Word") {
+      Animated.timing(slideAnimation, {
+        toValue: 230,
+        duration: 1000,
+      }).start();
+    } else if (changeable == "Definition") {
+      Animated.timing(slideAnimation, {
+        toValue: 0,
+        duration: 1000,
+      }).start();
+    }
   };
 
   function switchToDefinition() {
     console.log("Input entered.");
-    swing();
+    setChangable("Definition");
+    slide();
+  }
+
+  function goBack() {
+    setChangable("Word");
+    slide();
   }
 
   return (
@@ -31,11 +53,11 @@ export default function App() {
       >
         Kabbage!
       </Text>
-      <Animated.View style={[styles.subcontainer, { left: swingAnimation }]}>
-        {/* This view block contains everything besides Kabbage!*/}
-        <Text style={styles.textSub}>Word</Text>
+      <Animated.View style={[styles.subcontainer, { left: slideAnimation }]}>
+        <Text style={styles.textSub}>{changeable}</Text>
       </Animated.View>
       <View style={styles.subcontainer}>
+        {changeable == "Definition" && <Button title="Back" onPress={goBack} />}
         <TextInput style={styles.input} onChangeText={onInputChange} />
         <MyPressable disabled={!Input} onPress={switchToDefinition} />
       </View>
@@ -64,5 +86,13 @@ const styles = StyleSheet.create({
   textSub: {
     fontSize: 20,
     fontWeight: "300",
+  },
+  buttonInvisible: {
+    width: 0,
+    height: 0,
+  },
+  buttonVisible: {
+    height: 20,
+    width: 30,
   },
 });
