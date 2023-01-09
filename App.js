1f7710f18e9db2a44206b840c6a7c3f5712c5cb8
KMalveaux <KMalveaux0047@outlook.com>
2023-01-09 17:14:28 -0600

    Added the second stage of card creation Users can now pair definitions with their inputs

--- App.js
+++ App.js
@@ -10,13 +10,27 @@ import {
 } from "react-native";
 import MyPressable from "./myPressable";
 
+/*
+This is the "card creation" page of the app. It has two "stages". The input stage, where the user is entering a input, or the first part of the card, and the "definition" stage,
+where the user is entering a definition for their supplied input.
+
+The text input on this page works as follows. The user can only press enter when the text input is filled with any text. 
+When the user presses enter, a function is called that decides what should take place, either switching to the definition stage or creating the card, 
+by looking at the value of changeable. 
+
+Changeable is a state the can be seen visually on the page as the text that slides left to right above the text input when the user enters an input and presses enter, or the user presses 
+the go back button after supplying an input. Changeable should only flip between the strings "Word" and "Definition". Everytime changeable flips between the two strings, 
+the text input is replaced with a visually identical text input that is instead assigned with the according state for that stage of card creation, i.e. "onInputChange" for
+giving an input and then "onDefinitionChange" for giving a definition.
+*/
 export default function App() {
   const [Input, onInputChange] = useState(); // This is the first input the user gives. The "Word" to be given a defition.
+  const [Definition, onDefinitionChange] = useState();
   const [changeable, setChangable] = useState("Word"); //This is the text the changes between "word" and "definition"
   const slideAnimation = useRef(new Animated.Value(0)).current;
 
+  // An animation that moves the word "Word" or "Definition" to the left or to the right.
   const slide = () => {
-    // An animation that moves the word "Word" or "Definition" to the left or to the right.
     if (changeable == "Word") {
       Animated.timing(slideAnimation, {
         toValue: 230,
@@ -30,15 +44,40 @@ export default function App() {
     }
   };
 
-  function switchToDefinition() {
-    console.log("Input entered.");
-    setChangable("Definition");
-    slide();
+  // Decides what to do next when the enter button is pressed depending on the stage current stage of card creation.
+  function switchStage() {
+    if (changeable == "Word") {
+      setChangable("Definition");
+      console.log("Input entered: " + Input);
+      slide();
+    } else if (changeable == "Definition") {
+      // Finalize and create the card then go back to the input stage to begin creating a new card.
+      createCard();
+      goBack();
+    }
   }
 
+  // Called when the user presses the enter button after supplying a definition.
+  function createCard() {
+    console.log(
+      "Card created. Input = " + Input + ". Definition = " + Definition
+    );
+  }
+
+  //Called when the user presses the go back button that appears after supplying an input and pressing enter.
   function goBack() {
     setChangable("Word");
     slide();
+  }
+
+  //The visually identical input text with different states depending on the stage
+  var textField;
+  if (changeable == "Word") {
+    textField = <TextInput style={styles.input} onChangeText={onInputChange} />;
+  } else if (changeable == "Definition") {
+    textField = (
+      <TextInput style={styles.input} onChangeText={onDefinitionChange} />
+    );
   }
 
   return (
@@ -58,8 +97,8 @@ export default function App() {
       </Animated.View>
       <View style={styles.subcontainer}>
         {changeable == "Definition" && <Button title="Back" onPress={goBack} />}
-        <TextInput style={styles.input} onChangeText={onInputChange} />
-        <MyPressable disabled={!Input} onPress={switchToDefinition} />
+        {textField}
+        <MyPressable disabled={!Input} onPress={switchStage} />
       </View>
     </View>
   );
